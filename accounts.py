b1619433363a576b666c01ea900c6a085fdeacb2
Grace Smyth <gracemurphy45@gmail.com>
2024-03-12 11:27:51 +0000

    week 3 task

---
+++ accounts.py
@@ -0,0 +1,11 @@
+# Weekly Task 3
+# A python programme tha reads in a 10 character account number
+# but only outputs the account number with only the last 4 digits showing.
+
+# Author: Grace Mary Smyth
+
+account = input("account number: ")
+
+
+print("For security reasons your account number will be displayed as: ")
+print("XXXXXX",(account[6:10]))
