30c5eca60426bd295edc4286bc3633d424764067
Grace Smyth <gracemurphy45@gmail.com>
2024-03-16 18:53:24 +0000

    fixed odd and even numbers but still not working

--- collaty.py
+++ collaty.py
@@ -2,23 +2,29 @@
 # It then outputs the successive values of the calculation
 # The programme end if the current value is one
 # Author: Grace Mary Smyth
+# References: pythongeeks.org, w3schools.com
 
 number = int(input("Enter an integer: "))
 evennum = number/2
 oddnum = number*3 
 
 # If integer is even, divide by 2
-if (number / 2):
-    print(evennum)
+if (number%2==0):
+    print(int(evennum))
 
 # If integer is odd, multiply by 3
 else: 
-    print(oddnum)
+    print(int(oddnum))
 
 # A while loop
 while (number != 1):
-    number.append(number)
-     
+    continue
+    print [number]
+
+'''
 #if integer is one (end)
-if (number == 1): 
-    print ("End")
+if number == 1: 
+    break
+else:
+    print("End")
+    '''
