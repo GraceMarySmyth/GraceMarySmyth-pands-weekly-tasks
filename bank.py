c60f4c649f39e398a38e3c00ac5bacd6c024365e
Grace Smyth <gracemurphy45@gmail.com>
2024-03-16 19:19:42 +0000

    still attempting to fix bank

--- bank.py
+++ bank.py
@@ -6,10 +6,10 @@
 x = int(input("Enter amount1(in cent):"))
 
 y = int( input("Enter amount2(in cent):"))
-sum = sum([x,y])//100 # // gives the int division
-remainder = sum%100    # % gives the remainder
+euro = sum([x,y])//100 # // gives the int division
+remainder = euro%10    # % gives the remainder
 
 
-print(("Total: €", sum,remainder).format x, .y, sum, remainder)
+print("Total: €", euro,".",remainder)
 
 
