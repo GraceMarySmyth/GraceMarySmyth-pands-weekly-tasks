db337f50a0b953ca1873269ddb0eece3a33b9940
Grace Smyth <gracemurphy45@gmail.com>
2024-03-18 19:15:48 +0000

    another attempt

--- bank.py
+++ bank.py
@@ -7,9 +7,9 @@ x = int(input("Enter amount1(in cent):"))
 
 y = int( input("Enter amount2(in cent):"))
 euro = sum([x,y])//100 # // gives the int division
-remainder = euro%100   # % gives the remainder
+cent = euro%100   # % gives the remainder
 
 
-print("Total: €", euro,".",remainder)
+print("Total: €", euro,".",cent)
 
 
